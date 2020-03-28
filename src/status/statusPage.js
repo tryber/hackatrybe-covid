99423eafa6b38f4b0d0c41aee01ea56510b9d767
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-28 20:30:12 -0300

    Ajustando tamanho do mapa

--- src/status/statusPage.js
+++ src/status/statusPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 import { data } from "./locationData";
-import Circle from '../circle.png'
+import Circle from "../circle.png";
 
 class StatusPage extends React.Component {
   constructor(props) {
@@ -32,14 +32,14 @@ class StatusPage extends React.Component {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
-      showingInfoWindow: true,
+      showingInfoWindow: true
     });
 
   onMapClicked = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
-        activeMarker: null,
+        activeMarker: null
       });
     }
   };
@@ -61,11 +61,6 @@ class StatusPage extends React.Component {
     return { lat: currentName.latitude, lng: currentName.longitude };
   }
 
-//   calculeSize(deaths) {
-//     (n mortes / confirmados) * 100 > 5 
-//     (n mortes / confirmados) * 100 > 2
-//   }
-
   render() {
     console.log(this.state.apiData);
     if (this.state.apiData === "") {
@@ -73,36 +68,42 @@ class StatusPage extends React.Component {
     }
 
     return (
-      <Map
-        google={this.props.google}
-        zoom={5}
-        initialCenter={{ lat: -15.7801, lng: -47.9292 }}
-        style={{
-          width: "80%",
-          height: "80%"
-        }}
-        onClick={this.onMapClicked}
-        mapTypeControl={false}
-      >
-        {this.state.apiData.regions.map(region => (
-          <Marker
-            onClick={this.onMarkerClick}
-            position={this.findGeoLocation(region.name)}
-            name={`${region.name} - Confirmados: ${region.today_confirmed} - Mortes: ${region.today_deaths} - Recuperados: ${region.today_recovered}`}
-            icon= {{url: Circle, scaledSize: {width: Math.log(region.today_confirmed + 1)*15, height: Math.log(region.today_confirmed + 1)*15}}}
-            title={region.name}
-            opacity={0.6}
-          />
-        ))}
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
+      <div>
+        <h1>COVID-19 NO BRASIL</h1>
+        <Map
+          google={this.props.google}
+          zoom={5}
+          initialCenter={{ lat: -15.7801, lng: -47.9292 }}
+          onClick={this.onMapClicked}
+          mapTypeControl={false}
+          style={{width: '100%', height: '80%'}}
         >
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow>
-      </Map>
+          {this.state.apiData.regions.map(region => (
+            <Marker
+              onClick={this.onMarkerClick}
+              position={this.findGeoLocation(region.name)}
+              name={`${region.name} - Confirmados: ${region.today_confirmed} - Mortes: ${region.today_deaths} - Recuperados: ${region.today_recovered}`}
+              icon={{
+                url: Circle,
+                scaledSize: {
+                  width: Math.log(region.today_confirmed + 1) * 15,
+                  height: Math.log(region.today_confirmed + 1) * 15
+                }
+              }}
+              title={region.name}
+              opacity={0.6}
+            />
+          ))}
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+          >
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
     );
   }
 }
