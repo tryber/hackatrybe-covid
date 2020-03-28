7e58096185902cae71593ec5270589bcf19a5ef3
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-28 17:10:13 -0300

    Mapa funcionando

--- src/status/statusPage.js
+++ src/status/statusPage.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class statusPage extends React.Component {
-
+class StatusPage extends React.Component {
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={5}
+                initialCenter={{ lat: -15.7801, lng: -47.9292 }}
+                style={{
+                    width: '100%',
+                    height: '100%',
+                }}
+            />
+        );
+    }
 }
 
-export default statusPage;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyCWoGhhC5t7sdxEZg1h3ggFz24RWoFHzuE',
+})(StatusPage); 
