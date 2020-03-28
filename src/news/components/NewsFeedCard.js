8dd49ef5ed28c3d3d1e94c3be502133638cea45d
vrramosm <vf.ramos7@gmail.com>
2020-03-28 18:59:32 -0300

    Task Finalizada!

--- src/news/components/NewsFeedCard.js
+++ src/news/components/NewsFeedCard.js
@@ -2,21 +2,19 @@ import React, { useContext } from 'react';
 import context from '../context/context';
 
 function NewsFeed() {
-
   const { data } = useContext(context);
 
   return (
     <div>
       {data.map((articles) =>
-        <h3>{articles.title}</h3>
-        <p>{articles.description}</p>
+        <div>
+          <h3>{articles.title}</h3>
+          <img src={articles.urlToImage} alt={articles.title} />
+          <p>{articles.description}</p>
+          <a target="blank" href={articles.url}>Saiba Mais</a>
+          <h5>{articles.publishedAt}</h5>
+        </div>
       )}
-
-
-
-
-
-      {/* {data.map((articles) => )} */}
     </div>
   )
 }
