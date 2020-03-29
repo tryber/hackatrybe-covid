dbb00ae16387891ca0799dd19e6fa90184a3f637
henriqueeyer <henriqueeyer@outlook.com>
2020-03-28 22:39:25 -0300

    Correct format

--- src/remote_work/ListCardsContent.js
+++ src/remote_work/ListCardsContent.js
@@ -6,7 +6,6 @@ function ListCardsContent({ match: { params: { type } } }) {
   const contentFilter = content.filter((obj) => obj.type === type);
   return (
     <div className="List-Cards-Content">
-      <div>ListCardsContent</div>
       {contentFilter.map((obj) => <CardsContent key={obj.title} type={type} obj={obj} />)}
     </div>
   );
--- src/remote_work/PageRemoteWork.js
+++ src/remote_work/PageRemoteWork.js
@@ -4,9 +4,9 @@ import CardType from './CardType';
 function PageRemoteWork() {
   return (
     <div className="RemoteWork">
-      <CardType item={{ type: 'Professores', text: 'TEXTINHO SHOW DE BOLA' }} />
+      <CardType item={{ type: 'Professor', text: 'TEXTINHO SHOW DE BOLA' }} />
       <CardType item={{ type: 'Aluno', text: 'TEXTINHO SHOW DE BOLA' }} />
-      <CardType item={{ type: 'Ferramentas', text: 'TEXTINHO SHOW DE BOLA' }} />
+      <CardType item={{ type: 'Ferramenta', text: 'TEXTINHO SHOW DE BOLA' }} />
     </div>
   );
 }
