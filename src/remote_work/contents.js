24b024eb382f4af4bc41e410676059b5929d1db3
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-03-29 16:16:07 -0300

    Update on markdowns

--- src/remote_work/contents.js
+++ src/remote_work/contents.js
@@ -1,9 +1,13 @@
-import habitosAlunos from './contents/contentHabitosAlunos';
-import infraestrutura from './contents/infraestrutura';
+import infraestrutura from './contents/contentInfraestrutura';
+import ferramentasVisuais from './contents/contentFerramentasVisuais';
+import feedback from './contents/contentFeedback';
+import dinamicasInteracao from './contents/contentDinamicasInteracao';
 
 const content = [
-  habitosAlunos,
   infraestrutura,
-]
+  ferramentasVisuais,
+  feedback,
+  dinamicasInteracao
+];
 
 export default content;
