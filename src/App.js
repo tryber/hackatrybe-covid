cd413d866ce43dfcd7228db8caa9fb3b81026952
antoniopaolillo <antonio_francisco_p@live.com>
2020-03-29 12:47:58 -0300

    fixando o app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import StatusPage from './status/statusPage'
-import AboutUs from './about_us/SobreNos';
+// import AboutUs from './about_us/SobreNos';
 
 function App() {
   return (
     <div className="App">
-      {/* <div id='map'><StatusPage/></div> */}
-      <AboutUs />
+      <div id='map'><StatusPage/></div>
     </div>
   );
 }
