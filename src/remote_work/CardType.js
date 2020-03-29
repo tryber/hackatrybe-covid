d02abde5510e47005eb0180ae1078406f8691e62
henriqueeyer <henriqueeyer@outlook.com>
2020-03-29 11:31:57 -0300

    Finished css page RemoteWork

--- src/remote_work/CardType.js
+++ src/remote_work/CardType.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './CardType.css';
 
 function CardType({ item: { type, text } }) {
   return (
     <div className="Card-Type">
-      <h2>{type}</h2>
+      <h3>{type}</h3>
       <p>{text}</p>
       <Link class="button-card" to={`/remoteWork/${type}`}>Acessar</Link>
     </div>
--- src/remote_work/PageRemoteWork.js
+++ src/remote_work/PageRemoteWork.js
@@ -9,10 +9,13 @@ function PageRemoteWork() {
         <h1>Trabalho e Ensino Remoto</h1>
         <p>Dicas para adaptação a vida remota, boas praticas para trabalho e para o ensino a distancia!</p>
       </div>
+      <div className="content">
+        <h2>Conteúdos</h2>
       <div className="collection-card-type">
       <CardType item={{ type: 'Professor', text: 'Seção para os professores' }} />
       <CardType item={{ type: 'Aluno', text: 'Seção para os alunos' }} />
       <CardType item={{ type: 'Ferramenta', text: 'Ferramentas que usará' }} />
+      </div>
       </div>
     </div>
   );
